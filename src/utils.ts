f4111500ac5da215874aa4cb78b2b4aecf6c094f
Rafael <ikbenrafaelbieze2001@gmail.com>
2023-03-03 17:47:11 +0100

    With proxy magic

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,29 @@
 import { Base, NoInfer, PartialDeep } from "./types";
 
+const undefinedProperties = new Set<string | symbol>([
+  "nodeType",
+  "nodeName",
+  "isEqualNode",
+  "asymmetricMatch",
+]);
+
 /**
  * Lets you pass a deep partial of a type to a function
  *
  * @returns whatever you pass in
  */
-export const fromPartial = <T>(mock: PartialDeep<NoInfer<T>>): T => {
-  // const proxy = new Proxy(mock, {
-  //   get(target, p, receiver) {
-  //     if (typeof p !== "symbol" && !(p in target)) {
-  //       throw new Error(`${String(p)} not found in mocked object`);
-  //     }
-  //     return Reflect.get(target, p, receiver);
-  //   },
-  // });
-  return mock as T;
+export const fromPartial = <T>(mock: object & PartialDeep<NoInfer<T>>): T => {
+  const proxy = new Proxy(mock, {
+    get(target, p, receiver) {
+      if (undefinedProperties.has(p)) return undefined;
+
+      if (typeof p !== "symbol" && !(p in target)) {
+        throw new Error(`${String(p)} not found in mocked object`);
+      }
+      return Reflect.get(target, p, receiver);
+    },
+  });
+  return proxy as T;
 };
 
 /**
